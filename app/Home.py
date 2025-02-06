87196d8737c969fc6b579d233054920f1a65e4d9
Yubo Jing <jybjybjing@yubodeair.mynetworksettings.com>
2025-02-06 14:29:04 -0500

    google oauth

--- app/Home.py
+++ app/Home.py
@@ -134,7 +134,10 @@ google_credentials = {
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "redirect_uris": [st.secrets["google"]["redirect_uri"]]
+        "redirect_uris": [st.secrets["google"]["redirect_uri"]],
+        "javascript_origins": [
+            "https://food-ai.streamlit.app",
+            "http://localhost:8501"]
     }
 }
 
